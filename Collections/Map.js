d819a15c71da137f11e55083685efb1eb67ea424
viswarajramji <viswarajramji93@gmail.com>
2022-05-19 12:42:45 +0400

    update Collections

---
+++ Collections/Map.js
@@ -0,0 +1,42 @@
+//Syntax 
+// var map=new Map([Iterable(optional)]);
+var map=new Map();
+
+//property - Create operatosn
+//syntax : map.set(key,value)
+map.set("name","Shan");
+map.set("age","10");
+
+
+//Read Operations
+//entries() – returns a new Iterator object that contains an array of [key, value] for each element in the map object.The order of objects in the map is the same as the insertion order.
+//Get()- reads data from map
+console.log("***key data***");
+console.log(map.get("name"));
+console.log(map.get("test")); //returns undefined
+
+//keys()
+console.log("***keys***");
+for(const val of map.keys()){
+    console.log(val);
+}
+
+//values()
+console.log("*** values ***");
+for(const val of map.values()){
+    console.log(val);
+}
+
+//entries
+for(var value of map.entries()){
+    console.log(value[0]+"__"+value[1]);
+}
+
+//Delete operation
+//delete(key)
+console.log(map.delete("name"));
+console.log(map.get("name"));
+
+//clear - clears all the data
+map.clear();
+console.log(map.get("test"));
